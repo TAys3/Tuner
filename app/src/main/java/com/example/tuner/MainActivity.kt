eb1ac5750e6cd6ed4316142a83f23f1cde1272d2
Taylor <100389630+TAys3@users.noreply.github.com>
2023-05-11 07:20:26 +1000

    Handling microphone permissions - Not using deprecated functions anymore
    Duplicate classes problem solved

--- app/src/main/java/com/example/tuner/MainActivity.kt
+++ app/src/main/java/com/example/tuner/MainActivity.kt
@@ -1,34 +1,38 @@
 package com.example.tuner
 
+import android.Manifest
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import be.tarsos.dsp.io.android.AudioDispatcherFactory
+import be.tarsos.dsp.pitch.PitchDetectionHandler
+import be.tarsos.dsp.pitch.PitchProcessor
 import com.example.tuner.ui.theme.TunerTheme
 
 class MainActivity : ComponentActivity() {
+
+    val requestMicrophonePermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+//            Toast.makeText(applicationContext, "mic permission granted", Toast.LENGTH_SHORT).show()
+        } else {
+//            Toast.makeText(applicationContext, "mic permission refused", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestMicrophonePermission()
         setContent {
             TunerTheme {
                 // A surface container using the 'background' color from the theme
@@ -36,47 +40,67 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    MainWindow(pitch)
                 }
             }
         }
+        val dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(22050, 1024, 0)
+        val pdh = PitchDetectionHandler { res, e ->
+            val pitchInHz = res.pitch
+            runOnUiThread {
+                processPitch(pitchInHz)
+            }
+        }
+
+        val pitchProcessor =
+            PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN, 22050F, 1024, pdh)
+        dispatcher.addAudioProcessor(pitchProcessor)
+
+        val audioThread = Thread(dispatcher, "Audio Thread")
+        audioThread.start()
+    }
+
+    private fun requestMicrophonePermission() {
+        requestMicrophonePermissionLauncher.launch(Manifest.permission.RECORD_AUDIO)
     }
 }
 
-@Composable
-fun MainWindow() {
-    val cents = 0
-    val referencePitch = 440
-    Column() {
-        Row(modifier = Modifier.align(alignment = Alignment.CenterHorizontally)) {
-            Text(text = "#/b", Modifier.padding(8.dp))                                              //Will be an image that changes colour and sharp/flat
-            Text(text = "A", Modifier.padding(8.dp))                                                //Will just be a large letter in a text element
-            Text(text = "1", Modifier.padding(8.dp))                                                //Will also be text, but a lot smaller, might combine them if I can (with them each being different sizes)
-        }
-        Row(modifier = Modifier.align(alignment = Alignment.CenterHorizontally)) {
-            Text(text = "Bar goes here")                                                            //Will be an image with a rectangle on top?
-            //Marker goes here
-        }
-        Text(text = "$cents cents", modifier = Modifier.align(alignment = Alignment.CenterHorizontally))
-        Row(modifier = Modifier.align(alignment = Alignment.CenterHorizontally)) {
-            Button(onClick = { /*TODO*/ }) {
-//                Image(painter = painterResource(id = {}), contentDescription = null)
-                Text(text = "-")
-            }
-            Text(text = "$referencePitch")                                                          //Large text
-            Text(text = "Hz")                                                                       //Smaller text
-            Button(onClick = { /*TODO*/ }) {
-//                Image(painter = painterResource(id = {}), contentDescription = null)
-                Text(text = "+")
-            }
-        }
-    }
+
+
+
+private fun processPitch(pitchInHz: Float) {
+    //Process the pitch here
+    println(pitchInHz)
+    pitch = pitchInHz.toDouble()
 }
+
+var pitch = 0.0
+
+@Composable
+fun MainWindow(pitch: Double) {
+    Text(text = "$pitch")
+}
+
+@Composable
+fun PitchLetter() {
+
+}
+
+@Composable
+fun PitchAccuracy() {
+
+}
+
+@Composable
+fun ReferencePitch() {
+
+}
+
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ManWindowPreview() {
     TunerTheme {
-        MainWindow()
+        MainWindow(pitch)
     }
 }
